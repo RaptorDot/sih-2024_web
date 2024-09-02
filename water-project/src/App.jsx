aa5794a9662532d3e2387a9787da69828f22b1d7
Debasish <149887980+RaptorDot@users.noreply.github.com>
2024-09-03 00:30:14 +0530

    pro gross

--- water-project/src/App.jsx
+++ water-project/src/App.jsx
@@ -2,14 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { RouterProvider } from 'react-router-dom'
+
+import { createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Layout from './Layout'
 
 function App() {
   const [count, setCount] = useState(0)
-const router = ()
+const router = (
+  createRoutesFromElements(
+    <Route path='/' element={<Layout></Layout>}>
+
+    </Route>
+  )
+)
   return (
 <>
     <RouterProvider router={router}/>
+    
 </>
   )
 }
